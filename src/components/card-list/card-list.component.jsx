0932177c47fdbd0674cc2419cdcaa9960299ea9f
Ei Cho <eicho@unicomi.com>
2023-02-05 12:00:09 +0630

    search box

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,10 +1,12 @@
 import { Component } from "react";
+import "./card-list.styles.css";
 
 class CardList extends Component {
   render() {
+    // console.log(this.props.monsters);
     const { monsters } = this.props;
     return (
-      <div>
+      <div className="card-list">
         {monsters.map((monster) => (
           <h1 key={monster.id}>{monster.name}</h1>
         ))}

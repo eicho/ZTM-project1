c30e784aa414a2db4a9264e5d34ada895089ca4d
Ei Cho <eicho@unicomi.com>
2023-02-05 11:38:28 +0630

    change name by clicking button

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: { firstName: "Min", lastName: "Oo" },
+      company: "DBS",
+    };
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hi {this.state.name.firstName} {this.state.name.lastName},I work at{" "}
+            {this.state.company}
+          </p>
+          <button
+            onClick={() => {
+              this.setState(
+                () => {
+                  return { name: { firstName: "Tun", lastName: "Tun" } };
+                },
+                () => {
+                  console.log(this.state);
+                }
+              );
+            }}
+          >
+            Change Name
+          </button>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
